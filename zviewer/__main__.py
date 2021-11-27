1f870ba58601835353ed3227811ee4c7720849a7
lordidiot <calvin@arthurfong.com>
2021-11-27 17:12:58 +0800

    Update cli to make use of zendeskservice api

--- zviewer/__main__.py
+++ zviewer/__main__.py
@@ -1,7 +1,20 @@
+import argparse
+from re import L
 from zviewer.cli import ZViewerShell
+from zviewer.api import ZendeskService
 
 def main():
-    ZViewerShell().cmdloop()
+    parser = argparse.ArgumentParser(description='Ticket Viewer for Zendesk API.',
+                                     allow_abbrev=True)
+    parser.add_argument('subdomain', type=str,
+                        help='subdomain for your Zendesk workspace')
+    parser.add_argument('--user', '-u', type=str, help='username', required=True)
+    parser.add_argument('--pass', '-p', type=str, help='password', required=True)
+    args = parser.parse_args()
+    args = vars(args)
+    api_service = ZendeskService(args['subdomain'],
+                                 args['user'], args['pass'])
+    ZViewerShell(api_service).cmdloop()
 
 if __name__ == '__main__':
     main()
--- zviewer/cli.py
+++ zviewer/cli.py
@@ -1,18 +1,82 @@
+import re
 import cmd
 import zviewer
+from typing import Optional
+from zviewer import api
+from zviewer.api import ZendeskService, ZendeskServiceError
 
 class ZViewerShell(cmd.Cmd):
     intro = 'Welcome to Zendesk Viewer (zviewer).\nType help or ? to list commands.\n'
     prompt= '(zendesk) '
 
-    def do_connect(self, args):
-        raise NotImplementedError
+    def __init__(self, api_service: ZendeskService) -> None:
+        self.api_service = api_service
+        self.truncate_threshold = 50
+        # credit: https://stackoverflow.com/a/39520512
+        self.aliases = {'e': self.do_exit,
+                        'q': self.do_quit,
+                        'h': self.do_help}
 
-    def do_tickets(self, args):
-        raise NotImplementedError
+        super().__init__()
+
+    def do_tickets(self, _args):
+        try:
+            tickets = self.api_service.list_tickets()
+            start = 0
+            viewing = True
+            while viewing:
+                print("+"+"-"*60+"+")
+                for ticket in tickets[start:start+25]:
+                    self._preview_ticket(ticket)
+                    print("+"+"-"*60+"+")
+
+                if len(tickets) > 25:
+                    c = input(f"\nTickets {start+1}..{min(start+25,len(tickets))}/{len(tickets)}, press:\n\t(n) next page\n\t(p) prev page\n\t(q) to quit\n\t\n")
+                    if c.startswith('n') or not c:
+                        start = min(len(tickets)-25, start+25)
+                    elif c.startswith('p'):
+                        start = max(0, start-25)
+                    elif c.startswith('q'):
+                        viewing = False
+                print("")
+
+        except ZendeskServiceError as e:
+            print(f"ZendeskServiceError: {e}")
 
     def do_ticket(self, args):
-        raise NotImplementedError
+        try:
+            args = args.split()
+            if not args:
+                print("Usage: ticket id/id_range [id/id_range] ... ")
+            id_ranges = []
+            for param in args:
+                id_range = self._id_range(param)
+                if id_range:
+                    id_ranges.append(id_range)
+                else:
+                    print(f"id_range: '{param}' is invalid")
+            
+            ticket_ids = [
+                id 
+                for _range in id_ranges
+                for id in range(_range[0], _range[1]+1)
+            ]
+            self._display_tickets(ticket_ids)
+
+        except ZendeskServiceError as e:
+            print(f"ZendeskServiceError: {e}")
+
+    def _id_range(self, param: str) -> Optional[tuple[int, int]]:
+        if param.isdigit():
+            return (int(param), int(param))
+        elif re.match(r'\d+-\d+', param):
+            start, end = map(int, param.split('-'))
+            if start <= end:
+                return (start, end)
+            else:
+                return None
+        else:
+            return None
 
     def do_exit(self, args):
         return True
@@ -20,5 +84,56 @@ class ZViewerShell(cmd.Cmd):
     def do_quit(self, args):
         return True
 
-def parse(arg):
-    return tuple(map(int, arg.split()))
+    # credit: https://stackoverflow.com/a/39520512
+    def default(self, line):
+        cmd, arg, line = self.parseline(line)
+        if cmd and cmd in self.aliases:
+            return self.aliases[cmd](arg)
+        else:
+            print(f"*** Unknown syntax: {line}")
+    
+    def _truncate(self, s: str) -> str:
+        if len(s) <= self.truncate_threshold:
+            return s
+        else:
+            return s[:47]+'...'
+    
+    def _preview_ticket(self, ticket) -> None:
+        print(f"Ticket #{ticket['id']}")
+        print(f"Subject: {self._truncate(ticket['subject'])}")
+        print(f"Description:\n{self._truncate(ticket['description'])}")
+    
+    def _display_tickets(self, ticket_ids: list[int]) -> None:
+        print("")
+        if not ticket_ids:
+            return
+        elif len(ticket_ids) == 1:
+            ticket_id = ticket_ids[0]
+            ticket = self.api_service.get_ticket(ticket_id)
+            if not ticket:
+                print(f"Ticket #{ticket_id} not found.")
+            else:
+                self._print_ticket(ticket)
+            print("")
+        else:
+            retrieved = set()
+            requested = set(ticket_ids)
+            tickets = self.api_service.get_tickets(ticket_ids)
+            for ticket in tickets:
+                retrieved.add(ticket['id'])
+                self._print_ticket(ticket)
+                print("")
+            not_found = requested.difference(retrieved) 
+            if not_found:
+                print(f"Tickets {','.join(['#'+str(i) for i in not_found])} not found")
+
+    def _print_ticket(self, ticket):
+        print(f"Ticket #{ticket['id']}")
+        print(f"Subject: {ticket['subject']}")
+        print(f"Description:\n{ticket['description']}")
+        print("")
+        print(f"View ticket: [{self._ticket_to_url(ticket)}]")
+    
+    def _ticket_to_url(self, ticket) -> str:
+        url = self.api_service.get_url()
+        return f"{url}/agent/tickets/{ticket['id']}"
